725fdb96a3bd9210f47756169757b0aa054a900b
DariaAleshina <ddaleshina@gmail.com>
2025-07-30 19:24:25 +0200

    fetching data function added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,33 @@ import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
 import PageNotFound from './pages/PageNotFound';
 import './index.css';
-import AppNav from './components/AppNav';
 import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
+import { useEffect, useState } from 'react';
+import CityList from './components/CityList';
+
+const BASE_URL = 'http://localhost:9000';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        console.log(data);
+        setCities(data);
+      } catch {
+        alert('Error fetching cities data');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
@@ -16,7 +38,18 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from '../components/AppNav';
+import Map from '../components/Map';
+import Sidebar from '../components/SideBar';
 import styles from './AppLayout.module.css';
 
 function AppLayout() {
   return (
     <div className={styles.app}>
-      <AppNav />
+      <Sidebar />
+      <Map />
     </div>
   );
 }
